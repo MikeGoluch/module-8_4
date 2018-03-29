c6975086b387887fc9c6fdcc22cf4f9312234fa7
Michal Goluch <goluch.michal89@gmail.com>
2018-03-29 23:34:48 +0200

    add comments to the script

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+// inicjalizacja zmiennej
+
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+
+// alert('Hello, ' + name);
+
+// odpalenie kodu w konsoli
 console.log('Hello, ' + name);
